c40074208575b43cc219154dde3fed867c4f65df
bryandino673 <bdino673@gmail.com>
2024-10-31 15:26:06 +0100

    Updating: Implementing New Features in the server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const { resolve } = require("path");
 const env = require("dotenv").config({ path: "./.env" });
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-08-01",
+  apiVersion: "2024-04-10",
 });
 
 app.use(cors()); // Enable CORS for all origins (for development purposes)
