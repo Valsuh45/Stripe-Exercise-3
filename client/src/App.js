12e1cc3adc7d45b7a49d3a5db247da016d08c847
Boris <borisngha6@gmail.com>
2024-11-04 13:09:40 +0100

    Fix: fixing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <main>
-      <BrowserRouter>
+      <BrowserRouter basename="/Stripe-Exercise-3">
         <Routes>
           <Route path="/" element={<Payment />} />
           <Route path="/completion" element={<Completion />} />
